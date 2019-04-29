3259ad508966abd38a67261ef1ffb5492a8d655f
Emanoel Marques <emanoelrmsilva@gmail.com>
2019-04-29 16:25:06 -0300

    Novas funções

--- main.c
+++ main.c
@@ -1,149 +1,92 @@
 /*Síntese
-Objetivo: Fazer um CRUD de produtos utilizando vetores e registro sem função;
-Autor: Emanoel Roberto Marques da Silva;
-Data da ultima modificação: 06/03/2019;
+   Objetivo: CRUD de produtos utilizando todos os conhecimentos aprendidos até a prova P1.
+   Autor: Emanoel Roberto Marques da Silva.
+   Data: 29/04/2019;
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 #include <string.h>
+#define MAX 50
 
-#define QTD_PRODUTOS 10
-
-struct cadastro{
-	char nome[20];
-	float preco;
-	int codigo;
+struct produto{
+	int cod;
+	char nome[MAX];
+	float preco;	
 };
 
+int menuprincipal(void);
+void listarprod(struct produto prod[],int);
+void mostraprod(struct produto vet[],int cod);
+int buscaseq(struct produto vet[], int tamanho, int chave);
+int recebecod();
+void incluiprod(struct produto prod[],int *local);
+void excluiprod(struct produto prod[],int *local);
+
 int main(int argc, char *argv[]) {
- struct cadastro produtos[QTD_PRODUTOS],backup[QTD_PRODUTOS];
- int contador=0,buscaProdutos,opcao,i,flag=0,flagBackup=0,contadorBackup=0;
- 
- //ciclo do menu principal
- do{
- 	printf("		Menu Principal\n\n1- Verificar os produtos cadastrados.\n2- Cadastrar novo produto.\n3- Buscar um produto cadastrado.\n4- Excluir um produto.\n5- Alterar um produto.\n6- Salvar Backup.\n7- Restaurar Backup Salvo.\n0- Sair.\n");
-	scanf("\n%d",&opcao);
-	switch(opcao){
-		system("cls");
-		case 1:{
-			system("cls");
-			printf("Codigo		Descricao		Preco\n");
-			for(i=0;i<contador;i++){
-				printf("%d		%s			R$%2.f\n",produtos[i].codigo,produtos[i].nome,produtos[i].preco);
+	struct produto produtos[MAX],backup[MAX];
+	int qtdprod=0,opcao,flag=0,codbusca,i,contadorBackup,flagBackup;
+	setlocale(LC_ALL,"Portuguese");
+	do{
+		opcao=menuprincipal();
+		switch(opcao){
+			case 1:{
+				listarprod(produtos,qtdprod);
+				break;
 			}
-			system("pause");
-			break;
-		}
-		case 2:{
-			//cadastro de produtos
-			if(contador<QTD_PRODUTOS){
-				system("cls");
-				fflush(stdin);
-				printf("Digite o nome do produto: ");
-				gets(produtos[contador].nome);
-				do{
-					flag=0;
-					printf("Digite o Codigo do Produto: ");	
-					scanf("%d",&produtos[contador].codigo);
-					for(i=0;i<contador;i++){
-						if(produtos[contador].codigo == produtos[i].codigo){
-							system("cls");
-							printf("\nCodigo ja cadastrado no sistema, digite um codigo valido ou altere o produto ja cadastrado.\n");
-							flag=1;
-						}
-					}
-				}while(flag==1);
-				printf("Digite o Valor do Produto: ");	
-				scanf("%f",&produtos[contador].preco);
-				contador++;	
-				printf("Produto Cadastrado com sucesso!\n");
-				system("pause");
-				system("cls");
-			}else{
-				system("cls");
-				printf("\nNumero maximo de produtos cadastrados, Exclua algum produto para poder continuar a incluir novos produtos\n");
-			}
-			break;
-		}
-		case 3:{
-			//busca de produtos
-			printf("Digite o codigo do produto a ser buscado: ");
-			scanf("%d",&buscaProdutos);
-			for(i=0;i<contador;i++){
-				if(buscaProdutos == produtos[i].codigo){
-					printf("Produto encontrado\nCodigo: %d\nDescricao: %s\nPreco: %2.f\n",produtos[i].codigo,produtos[i].nome,produtos[i].preco);
-					system("pause");
-					system("cls");
-					flag=1;
+			case 2:{
+				system("CLS");
+				printf("--------------------Busca-----------\n");
+				codbusca=recebecod();
+				flag=buscaseq(produtos,qtdprod,codbusca);
+				if(flag == -1){
+					system("CLS");
+					printf("Código buscado não encontrado\n");
+				}else{
+					mostraprod(produtos,flag);
 				}
+				break;
 			}
-			if(flag==0){
-				printf("Codigo digitado nao pertence a um produto cadastrado.\n");
-				system("pause");
-				system("cls");
+			case 3:{
+				produtos[qtdprod].cod=recebecod();
+				incluiprod(produtos,&qtdprod);
+				break;
 			}
-			break;
-		}
-		case 4:{
-			//excluir um produto
-			system("cls");
-			printf("Digite o codigo do produto a ser excluido: ");
-			scanf("%d",&buscaProdutos);
-			for(i=0;i<contador;i++){
-				if(buscaProdutos == produtos[i].codigo){
-					produtos[i].codigo=produtos[contador-1].codigo;
-					strcpy(produtos[i].nome,produtos[contador-1].nome);
-					produtos[i].preco=produtos[contador-1].preco;
-					printf("\nProduto excluido com sucesso\n");
-					contador--;
-					system("pause");
-					system("cls");
-					flag=1;
-					
-				}
+			case 4:{
+				system("CLS");
+				printf("ALTERAÇÃO\n");
+				codbusca=recebecod();
+				flag=buscaseq(produtos,qtdprod,codbusca);
+				if(flag == -1){
+					system("CLS");
+					printf("Código buscado não encontrado\n");
+				}else{
+					incluiprod(produtos,&flag);
+				}	
+				break;
 			}
-			if(flag==0){
-				printf("Codigo digitado nao pertence a um produto cadastrado.\n");
-				system("pause");
-				system("cls");
+			case 5:{
+				system("CLS");
+				printf("Exclusão\n");
+				codbusca=recebecod();
+				flag=buscaseq(produtos,qtdprod,codbusca);
+				if(flag == -1){
+					system("CLS");
+					printf("Código buscado não encontrado\n");
+				}else{
+					excluiprod(produtos,&qtdprod);
+				}	
+				break;
 			}
-			break;
-		}
-		case 5:{
-			//alterar nome e preço de um produto
-			system("cls");
-			printf("Digite o codigo do produto a ser alterado: ");
-			scanf("%d",&buscaProdutos);
-			for(i=0;i<contador;i++){
-				if(buscaProdutos == produtos[i].codigo){
-					fflush(stdin);
-					printf("\nDigite o nome do produto: ");
-					gets(produtos[i].nome);
-					printf("\nDigite o preco do produto: ");
-					scanf("%f",&produtos[i].preco);
-					i=contador;
-				}
-				printf("\nProduto alterado com sucesso\n");
-				system("pause");
-				system("cls");
-				flag=1;
-			}
-			if(flag==0){
-				printf("Codigo digitado nao pertence a um produto cadastrado.\n");
-				system("pause");
-				system("cls");
-			}
-			break;
-		}
-		case 6:{
+			case 6:{
 			//Backup
-			for(i=0;i<contador;i++){
-				backup[i].codigo=produtos[i].codigo;
+			for(i=0;i<qtdprod;i++){
+				backup[i].cod=produtos[i].cod;
 				strcpy(backup[i].nome,produtos[i].nome);
 				backup[i].preco=produtos[i].preco;
 			}
 			system("cls");
-			contadorBackup=contador;
+			contadorBackup=qtdprod;
 			printf("Backup Concluido!\n");
 			flagBackup=1;
 			break;
@@ -156,12 +99,12 @@ int main(int argc, char *argv[]) {
 			switch(opcao){
 				case 1:{
 					if(flagBackup==1){
-						for(i=0;i<contador;i++){
-							produtos[i].codigo=backup[i].codigo;
+						for(i=0;i<qtdprod;i++){
+							produtos[i].cod=backup[i].cod;
 							strcpy(produtos[i].nome,backup[i].nome);
 							produtos[i].preco=backup[i].preco;
 						}
-						contador=contadorBackup;
+						qtdprod=contadorBackup;
 						printf("\nBackup restaurado com sucesso!\n");
 					}else{
 						system("cls");
@@ -182,19 +125,80 @@ int main(int argc, char *argv[]) {
 				}
 			break;
 		}
-		case 0:{
-			//mensagem de término
-			system("cls");
-			printf("Aplicacao finalizada com sucesso!");
-			break;
 		}
-		default:{
-			//mensagem de opção inválida
-			system("cls");
-			printf("Opcao invalida, digite uma opcao valida.\n\n");
-			break;
-		}
-	}
-}while(opcao!=0);
+	}while(opcao!=0);
+	
 	return 0;
 }
+
+int menuprincipal(void){
+	int op;
+	printf("	MENU");
+	printf("\n1- Listar.");
+	printf("\n2- Consultar.");
+	printf("\n3- Incluir.");
+	printf("\n4- Alterar.");
+	printf("\n5- Excluir.");
+	printf("\n6- Salvar Backup.");
+	printf("\n7- Retomar Ponto Salvo.");
+	printf("\n0- Sair.\n");
+	scanf("%d",&op);
+	if(op>=0 && op<8)
+		return op;
+	else{
+		system("CLS");
+		printf("\nOpção inválida.\n");
+		return menuprincipal();
+	}
+}
+
+void listarprod(struct produto prod[],int qt){
+	int i;
+	if(qt>0){
+		printf("\nCódigo			Nome			Preço\n");
+		for(i=0;i<qt;i++){
+			printf("%d			%s				R$%.2f\n",prod[i].cod,prod[i].nome,prod[i].preco);
+		}
+	}else
+		printf("\nNão há nenhum produto cadastrado atualmente.\n");
+}
+
+int buscaseq(struct produto vet[], int tamanho, int chave) {
+	int i=0;
+	while ((vet[i].cod < chave) && (i < tamanho)) {
+		i++;
+	}
+	if (vet[i].cod == chave){
+		return i;
+	}else{
+		return -1;
+	}
+}
+
+int recebecod(){
+	int i;
+	printf("Digite o código: ");
+	scanf("%d",&i);
+	return i;
+}
+
+void mostraprod(struct produto vet[],int cod){
+	system("CLS");
+	printf("Encontrado\nCódigo: %d\nNome: %s\nPreço:%.2f",vet[cod].cod,vet[cod].nome,vet[cod].preco);
+}
+
+void incluiprod(struct produto prod[],int *local){
+	fflush(stdin);
+	printf("Digite o Nome: ");
+	gets(prod[*local].nome);
+	printf("Digite o Preço: ");
+	scanf("%f",&prod[*local].preco);
+	(*local)++;
+}
+
+void excluiprod(struct produto prod[],int *local){
+	prod[*local] = prod[*local-1];
+	(*local)--;
+	printf("\nExcluído com sucesso.");
+}
+
